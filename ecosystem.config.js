8577c483d08c633ba978d9c9b6f9294cb2b4c33d
Indra Kumar Prajapat <indrakumarprajapat@gmail.com>
2026-01-07 14:49:16 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,20 +3,13 @@ module.exports = {
     {
       name: "Aavin-PG-Handler",
       script: "node_modules/next/dist/bin/next",
-      args: "start -p 3003",
+      args: "start -H 0.0.0.0 -p 3003",
       cwd: "/home/ec2-user/var/www/uat/AavinPGHandlerWebApp",
       exec_mode: "fork",
       instances: 1,
-      autorestart: true,
       env: {
-        NODE_ENV: 'development',
-                PORT: 3003
-      },
-      env_production: {
-        NODE_ENV: 'production',
-                PORT: 3003
-
-      },
-    },
-  ],
+        NODE_ENV: "production"
+      }
+    }
+  ]
 };
