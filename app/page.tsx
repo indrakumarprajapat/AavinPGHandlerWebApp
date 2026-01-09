ef3e1ea800afb8767a91e84017514f4aeda27e7a
Indra Kumar Prajapat <indrakumarprajapat@gmail.com>
2026-01-09 13:00:10 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -21,11 +21,8 @@ export default function Home({ searchParams }: HomeProps) {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Aavin CBE Portal
+            Aavin 
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Welcome to Aavin CBE Payment Portal. This page handles payment redirects and confirmations.
-          </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
